d04a3a5e16959f14b129e8c41c8ceff302856937
jeff dorso <1evolutionlife@gmail.com>
2020-09-15 20:10:42 -0400

    updated app with shop page, collection preview componenet and collection item component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import {Switch, Route } from 'react-router-dom';
 
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
-import ShopPage from './pages/shop/shop.component';
+import ShopPage from './pages/shop/shop.component.jsx';
+
 
 
 function App() {
