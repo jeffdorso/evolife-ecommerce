064a9e466a99816c39d9ac8f4ab66f36d2d7a42a
jeff dorso <1evolutionlife@gmail.com>
2020-11-12 08:01:08 -0500

    added firebase to application and added google sign in to application

--- src/App.js
+++ src/App.js
@@ -6,22 +6,48 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
 import Header from './components/header/header.component.jsx';
 import SignInSignUpPage from './pages/sign-in-sign-up/sign-in-sign-up.component.jsx';
+import { auth } from './firebase/firebase.utils'; 
 
 
 
+class  App extends React.Component  {
 
-function App() {
-  return (
-    <div >
-      <Header/>
-      <Switch>
-        <Route exact path='/' component={HomePage}/>
-        <Route  path='/shop' component={ShopPage}/>
-        <Route path='/signin' component={SignInSignUpPage}/>
-      
-      </Switch>
-    </div>
-  );
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null
+    };
+  }
+
+  unsubscribeFromAuth = null
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user});
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render () {
+    return (
+      <div >
+        <Header currentUser={this.state.currentUser} />
+        <Switch>
+          <Route exact path='/' component={HomePage}/>
+          <Route  path='/shop' component={ShopPage}/>
+          <Route path='/signin' component={SignInSignUpPage}/>
+        
+        </Switch>
+      </div>
+    );
+  }
+
+
+ 
 }
 
 export default App;
--- src/components/sign-in/sign-in.components.jsx
+++ src/components/sign-in/sign-in.components.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
+import { signInWithGoogle } from '../../firebase/firebase.utils';
+
 import './sign-in.styles.scss';
 
 class SignIn extends React.Component{
@@ -42,10 +44,16 @@ class SignIn extends React.Component{
                         value={this.state.email}
                         handleChange={this.handleChange}
                         label='password'
-                         required/>
-                        
-
+                         required
+                         />
+                        <div className='buttons'>
                         <CustomButton type="submit"> Sign In </CustomButton> 
+                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn> 
+                        {' '}
+                        Sign In With Google{' '}
+                         </CustomButton> 
+                        </div>
+                       
                     </form>
                 </div>
             );
