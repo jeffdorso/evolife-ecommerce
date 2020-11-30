884e4d477a2ac946f825971f4b5737be9f54dab9
jeff dorso <1evolutionlife@gmail.com>
2020-11-30 07:57:18 -0500

    update application with reselect, replaced cart selector with memoized selector

--- src/components/cart-icon/cart-icon.components.jsx
+++ src/components/cart-icon/cart-icon.components.jsx
@@ -7,10 +7,11 @@ import { ReactComponent as ShoppingIcon } from '../../assets/ecommerce_cart.svg'
 
 import './cart-icon.styles.scss';
 
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
     <div className='cart-icon' onClick={ toggleCartHidden }>
         <ShoppingIcon className='shopping-icon'/>
-        <span className='item-count'>0</span>
+        <span className='item-count'>{itemCount}
+        </span>
     </div>
 );
 
@@ -18,4 +19,12 @@ const mapDispatchToProps = dispatch => ({
     toggleCartHidden: () => dispatch( toggleCartHidden() )
 });
 
-export default connect(null, mapDispatchToProps) (CartIcon);
+// selector - increases the cart count 
+const mapStateToProps = ({ cart: {cartItems} }) => ({
+    itemCount: cartItems.reduce(
+      (accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity, 0 
+
+    )
+});
+
+export default connect(mapStateToProps, mapDispatchToProps) (CartIcon);
